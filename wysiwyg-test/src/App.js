8c4128e115141c47e6c03127e83fd149d29b6583
veronicarubim <veronica.rubim97@gmail.com>
2023-04-17 22:46:07 -0300

    editor de texto - quill-react

--- wysiwyg-test/src/App.js
+++ wysiwyg-test/src/App.js
@@ -1,28 +1,11 @@
-import ClassicEditor from "@ckeditor/ckeditor5-build-classic"
-import {CKEditor} from "@ckeditor/ckeditor5-react"
-import parse from "html-react-parser"
-import { useEffect, useState } from "react";
+import Editor from "./components/editor/Editor";
+
+
 
 function App() {
-  const [text, setText] = useState("")
-  console.log(text)
-
   return (
     <div className="App">
-      <div className="editor">
-        <CKEditor
-          editor={ClassicEditor}
-          data={text}
-          onChange={(event, editor) => {
-            const data = editor.getData()
-            setText(data)
-          }}
-        />
-      </div>
-      <div>
-        <h2>Content</h2>
-        <p>{parse(text)}</p>
-      </div>
+      <Editor></Editor>
     </div>
   );
 }
